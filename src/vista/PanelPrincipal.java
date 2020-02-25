d99b31eaec614ce823c2f1fc4b827adfab8515ee
Juan-Pe13 <56963804+Juan-Pe13@users.noreply.github.com>
2020-02-25 21:05:39 +0100

    Cambios estéticos de la vista

--- src/vista/PanelPrincipal.java
+++ src/vista/PanelPrincipal.java
@@ -4,6 +4,8 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import main.Controlador;
 
@@ -16,18 +18,30 @@ public class PanelPrincipal extends JFrame{
 	public PanelPrincipal(Controlador c) {
 		super("PROGRAMACION EVOLUTIVA");
 		
+		rp = new RightPanel(this, c, gp);	
+		
+		this.setLayout(new GridBagLayout());	
+		GridBagConstraints constraints = new GridBagConstraints();
+		
+		constraints.gridx = 5;
+		constraints.gridy = 2;
+		constraints.weightx = 1;
+		constraints.weighty = 1;	// 
+		constraints.fill = GridBagConstraints.VERTICAL;
+		constraints.anchor = GridBagConstraints.EAST;
+		this.add(rp, constraints);
+			
 		gp = new GraficPanel(this, c);
-	//	gp.setSize(500, 500);
-		rp = new RightPanel(this, c, gp);
 		
-		add(gp, BorderLayout.EAST);
-		add(rp);
-	
+		constraints.gridx = 3;
+		constraints.gridy = 2;
+		constraints.weightx = 0;
+		constraints.weighty = 1;	// 
+		constraints.anchor = GridBagConstraints.WEST;
+		constraints.fill = GridBagConstraints.VERTICAL;
+			
+		this.add(gp, constraints);
 		
-		//add(rp, BorderLayout.CENTER);
-		//add(gp, BorderLayout.EAST);
-				
-	//	setLayout(new GridLayout(1, 3));	
 		this.setSize(1300, 700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		
