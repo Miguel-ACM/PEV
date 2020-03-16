1e2ac97cc41c9afe1b19924a528bd37780c69229
Miguel Ángel <miguelangel@MacBook-Air-de-admin.local>
2020-03-16 17:07:25 +0100

    Lectura de archivos (sin probar)

--- G08P1/fitness/FitnessHospital.java
+++ G08P1/fitness/FitnessHospital.java
@@ -1,15 +1,47 @@
 package fitness;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import individuo.Individuo;
 
 public class FitnessHospital implements Fitness{
 	
-	public float[][] flujos;
-	public float[][] distancias;
+	private int[][] flujos;
+	private int[][] distancias;
+	private int size;
+	
+	private int[][] readMatrix(Scanner sc){
+		int[][] res = new int[size][size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			String[] numbers = sc.nextLine().split(" ");
+			for (int j = 0; j < numbers.length; j++)
+			{
+				res[i][j] = Integer.parseInt(numbers[j]); //No lo he mirado, a lo mejor este es distancias
+			}
+		}
+		return res;
+	}
 	
 	public FitnessHospital(String filepath)
 	{
-		
+		File file = new File("archivos" + filepath); 
+	    Scanner sc;
+		try {
+			sc = new Scanner(file);
+			size = Integer.parseInt(sc.nextLine());
+			sc.nextLine(); //Linea en blanco
+			flujos = readMatrix(sc);
+			sc.nextLine(); //Linea en blanco
+			distancias = readMatrix(sc);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+	  
+	    
 	}
 	
 	@Override
